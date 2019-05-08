1ec73cba9b854652c873780dade5bfde915916d8
Siggy <s_gyi@yahoo.com>
2019-05-08 14:53:38 -0400

    Added scenes to test fixed joints, hinge joints, and the creation of a joint upon collision.

---
+++ Assets/Scripts/collisionTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collisionTest : MonoBehaviour
+{
+    public float thrust;
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        print(collision.contacts[0].point);
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            rb.AddForce(transform.forward * thrust);   
+        }
+    }
+}
---
+++ Assets/Scripts/jointOnCollision.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jointOnCollision : MonoBehaviour
+{
+    public float thrust;
+    Rigidbody rb;
+    Rigidbody otherrb;
+    FixedJoint fj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        print(collision.contacts[0].point);
+        print("collison.gameObject:" + collision.gameObject);
+        print("this.gameObject:" + this.gameObject);
+
+        otherrb = collision.gameObject.GetComponent<Rigidbody>();
+        fj = this.gameObject.AddComponent<FixedJoint>();
+        fj.connectedBody = otherrb;
+
+        print("fj.connectedAnchor: "+ fj.connectedAnchor);
+        print("fj.Anchor: "+ fj.anchor);
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            rb.AddForce(transform.forward * thrust);   
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rb.AddForce(transform.up * thrust);
+        }
+    }
+}
